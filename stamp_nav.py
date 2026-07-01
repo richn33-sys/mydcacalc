54bd0bd31ac39ebc9ac826ef4acf4edde4ce7155
richard nashawaty <richn33@gmail.com>
2026-07-01 07:32:25 -0400

    Add 529 college savings calculator

--- stamp_nav.py
+++ stamp_nav.py
@@ -29,6 +29,7 @@ CALC_CATEGORIES = [
         ('dca-backtest.html',                   'DCA backtest'),
         ('compound-interest.html',              'Compound interest'),
         ('rule-of-72-calculator.html',          'Rule of 72'),
+        ('529-calculator.html',                 '529 college savings'),
         ('loss-recovery-calculator.html',       'Loss recovery'),
         ('breakeven-calculator.html',           'Breakeven'),
         ('crypto-cost-basis-calculator.html',   'Crypto cost basis'),
